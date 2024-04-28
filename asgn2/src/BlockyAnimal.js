709c5a5ec646d65ccd47e62e50f67d6dcc1418a9
Danny <dchan27@ucsc.edu>
2024-04-28 00:59:12 -0700

    pushing what i have so far

--- asgn2/src/BlockyAnimal.js
+++ asgn2/src/BlockyAnimal.js
@@ -93,6 +93,9 @@ let g_numSegments = 100;
 let g_yellowAngle = 0;
 let g_magentaAngle = 0;
 let g_globalAngle = 0;
+let g_x = 0;
+let g_Yangle = 0;
+let g_yAngle = 0;
 let g_yellowAnimation=false;
 let g_magentaAnimation=false;
 
@@ -128,7 +131,13 @@ function main() {
   addActionsForHtmlUI(); 
 
   // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = click;
+  canvas.onmousedown = function (e) {
+    dragging = true;
+    let [x,y] = convertCoordinatesEventToGL(e);
+    g_lastX = x;
+    g_lastY = y;
+  }
+  // canvas.onmousedown = click;
   // canvas.onmousemove = click;
   canvas.onmousemove = function(ev) {if(ev.buttons == 1) { click(ev) } };
 
@@ -167,26 +176,25 @@ var g_shapesList = [];
 function click(ev) {
 
   // Extract the event click and return it in WebGL coordinates
-  [x,y] = convertCoordinatesEventToGL(ev);
+  let [x,y] = convertCoordinatesEventToGL(ev);
+  // cited by watching youtube videos of watching mouse control on webgl
+  if (dragging) {
+    var dx = 360 * (x - g_lastX);
+    var dy = 360 * (y - g_lastY)
 
-  // create and store the new point
-  let point;
-  if(g_selectedType == POINT) {
-    point = new Point();
-  } else if (g_selectedType == TRIANGLE) {
-    point = new Triangle();
-  } else {
-    point = new Circle();
-    point.segments = g_numSegments;
+    g_x += dy;
+    g_Yangle += dx;
+    if (Math.abs(g_globalAngle / 360) > 1){
+      g_x = 0;
+    }
+    if (Math.abs(g_yAngle / 360) > 1){
+      g_Yangle = 0;
+    }
+
   }
-  point.position = [x,y];
-  point.color = g_selectedColor.slice();
-  point.size = g_selectedSize;
-  g_shapesList.push(point);
-
-  // draw every shape that is supposed to be in the canvas 
-  renderAllShapes();
-
+  g_lastX = x;
+  g_lastY = y;
+ 
 }
 
 // Extract the event click and return it in WebGL coordinates
@@ -209,53 +217,237 @@ function renderAllShapes(){
   var startTime = performance.now();
 
   var globalRotMat=new Matrix4().rotate(g_globalAngle,0,1,0);
+  globalRotMat.rotate(g_yAngle, 1 , 0 ,0);
+  globalRotMat.rotate(g_Yangle, 1 , 0 ,0);
+  globalRotMat.rotate(g_x, 0, 1 ,0);
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
   // // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
+  renderSnorlax();
+
   // drawTriangle3D([-1.0,0.0,0.0, -0.5,-1.0,0.0, 0.0,0.0,0.0]);
 
-  var body = new Cube();
-  body.color = [1.0,0.0,0.0,1.0];
-  body.matrix.translate(-.25,-.75,0.0);
-  body.matrix.rotate(-5,1,0,0);
-  body.matrix.scale(0.5,.3,.5);
-  body.render();
+  // var body = new Cube();
+  // body.color = [1.0,0.0,0.0,1.0];
+  // body.matrix.translate(-.25,-.75,0.0);
+  // body.matrix.rotate(-5,1,0,0);
+  // body.matrix.scale(0.5,.3,.5);
+  // body.render();
 
-  var yellow = new Cube();
-  yellow.color = [1,1,0,1];
-  yellow.matrix.setTranslate(0,-.5,0.0);
-  yellow.matrix.rotate(-5,1,0,0);
-  yellow.matrix.rotate(-g_yellowAngle,0,0,1); 
-  var yellowCoordinatesMat = new Matrix4(yellow.matrix);
-  yellow.matrix.scale(0.25,.7,.5);
-  yellow.matrix.translate(-.5,0,0);
-  yellow.render();
+  // var yellow = new Cube();
+  // yellow.color = [1,1,0,1];
+  // yellow.matrix.setTranslate(0,-.5,0.0);
+  // yellow.matrix.rotate(-5,1,0,0);
+  // yellow.matrix.rotate(-g_yellowAngle,0,0,1); 
+  // var yellowCoordinatesMat = new Matrix4(yellow.matrix);
+  // yellow.matrix.scale(0.25,.7,.5);
+  // yellow.matrix.translate(-.5,0,0);
+  // yellow.render();
 
-  var box = new Cube();
-  box.color = [1,0,1,1];
-  box.matrix = yellowCoordinatesMat;
-  box.matrix.translate(0,0.65,0);
-  box.matrix.rotate(g_magentaAngle,0,0,1);
-  box.matrix.scale(.3,.3,.3);
-  box.matrix.translate(-.5,0,-.001);
-  box.render();
+  // var box = new Cube();
+  // box.color = [1,0,1,1];
+  // box.matrix = yellowCoordinatesMat;
+  // box.matrix.translate(0,0.65,0);
+  // box.matrix.rotate(g_magentaAngle,0,0,1);
+  // box.matrix.scale(.3,.3,.3);
+  // box.matrix.translate(-.5,0,-.001);
+  // box.render();
 
-  var K = 10.0;
-  for (var i = 1; i < K; i++) {
-    var c = new Cube();
-    c.matrix.translate(-.8,1.9*i/K-1.0,0);
-    c.matrix.rotate(g_seconds*100,1,1,1);
-    c.matrix.scale(.1,0.5/K,1.0/K);
-    c.render();
-  }
+  // var K = 10.0;
+  // for (var i = 1; i < K; i++) {
+  //   var c = new Cube();
+  //   c.matrix.translate(-.8,1.9*i/K-1.0,0);
+  //   c.matrix.rotate(g_seconds*100,1,1,1);
+  //   c.matrix.scale(.1,0.5/K,1.0/K);
+  //   c.render();
+  // }
 
   // check the time at the end of the function and show on webpage
   var duration = performance.now() - startTime;
   sendTextToHTML("ms: " + Math.floor(duration) + " fps: "  + Math.floor(10000/duration)/10, "numdot");
 
+}
+
+
+// reference: 
+// https://static.wikia.nocookie.net/p-quest/images/7/7d/143.png/revision/latest?cb=20180824081535
+function renderSnorlax() {
+  var snorlaxColor = [95/255,150/255,170/255,1.0];
+  var snorlaxFurColor = [241/255,229/255,215/255,0.95];
+  var snorlaxFeetColor = [241/255,229/255,215/255,1.0];
+  var snorlaxEyeMouthColor = [0/255,0/255,0/255,1.0];
+  var snorlaxPawColor = [206/255, 194/255, 184/255, 1.0];
+  var snorlaxNailColor = [255/255,255/255,255/255,1.0];
+
+  var snorlaxBody = new Cube();
+  snorlaxBody.color = snorlaxColor;
+  snorlaxBody.matrix.rotate(0,0,0,1);
+  snorlaxBody.matrix.scale(1,1,.9);
+  snorlaxBody.matrix.translate(-.5,-.8,-.001);
+  snorlaxBody.render();
+
+  var snorlaxBodyFur = new Cube();
+  snorlaxBodyFur.color = snorlaxFurColor;
+  snorlaxBodyFur.matrix = new Matrix4(snorlaxBody.matrix);
+  snorlaxBodyFur.matrix.rotate(0,0,0,1);
+  snorlaxBodyFur.matrix.scale(.8,.9,.04);
+  snorlaxBodyFur.matrix.translate(0.13,0.1,-.9);
+  snorlaxBodyFur.render();
+
+  var snorlaxHead = new Cube();
+  snorlaxHead.matrix.rotate(0,0,0,1);
+  snorlaxHead.matrix.scale(.8,.45,.8);
+  snorlaxHead.matrix.translate(-.497,.445,.065);
+  snorlaxHead.color = snorlaxColor;
+  snorlaxHead.render();
+
+  var snorlaxFace = new Cube();
+  snorlaxFace.color = snorlaxFurColor;
+  snorlaxFace.matrix = new Matrix4(snorlaxHead.matrix);
+  snorlaxFace.matrix.rotate(0,0,0,1);
+  snorlaxFace.matrix.scale(.8,.93,.04);
+  snorlaxFace.matrix.translate(0.14,-0.15,-.9);
+  snorlaxFace.render();
+
+  var snorlaxLeftEye = new Cube();
+  snorlaxLeftEye.color = snorlaxEyeMouthColor;
+  snorlaxLeftEye.matrix = new Matrix4(snorlaxFace.matrix);
+  snorlaxLeftEye.matrix.rotate(0,0,0,1);
+  snorlaxLeftEye.matrix.scale(.2,.05,.02);
+  snorlaxLeftEye.matrix.translate(0.8,14,-3);
+  snorlaxLeftEye.render();
+
+  var snorlaxRightEye = new Cube();
+  snorlaxRightEye.color = snorlaxEyeMouthColor;
+  snorlaxRightEye.matrix = new Matrix4(snorlaxFace.matrix);
+  snorlaxRightEye.matrix.rotate(0,0,0,1);
+  snorlaxRightEye.matrix.scale(.2,.05,.02);
+  snorlaxRightEye.matrix.translate(3.2,14,-3);
+  snorlaxRightEye.render();
+
+  var snorlaxMouth = new Cube();
+  snorlaxMouth.color = snorlaxEyeMouthColor;
+  snorlaxMouth.matrix = new Matrix4(snorlaxFace.matrix);
+  snorlaxMouth.matrix.rotate(0,0,0,1);
+  snorlaxMouth.matrix.scale(.4,.05,.02);
+  snorlaxMouth.matrix.translate(.75,7,-3);
+  snorlaxMouth.render();
+
+  // named from my pov so snorlax {position} {feature} is technically mirrored
+  var snorlaxsLeftEar = new Cube();
+  snorlaxsLeftEar.matrix = new Matrix4(snorlaxHead.matrix);
+  snorlaxsLeftEar.matrix.rotate(0,0,0,1);
+  snorlaxsLeftEar.matrix.scale(.2,.3,.2);
+  snorlaxsLeftEar.matrix.translate(0.5,3.3,2);
+  snorlaxsLeftEar.color = snorlaxColor;
+  snorlaxsLeftEar.render();
+
+  var snorlaxsRightEar = new Cube();
+  snorlaxsRightEar.matrix = new Matrix4(snorlaxHead.matrix);
+  snorlaxsRightEar.matrix.rotate(0,0,0,1);
+  snorlaxsRightEar.matrix.scale(.2,.3,.2);
+  snorlaxsRightEar.matrix.translate(3.5,3.3,2);
+  snorlaxsRightEar.color = snorlaxColor;
+  snorlaxsRightEar.render();
+
+  var snorlaxsLeftArm = new Cube();
+  snorlaxsLeftArm.matrix = new Matrix4(snorlaxBody.matrix);
+  snorlaxsLeftArm.matrix.rotate(0,0,0,1);
+  snorlaxsLeftArm.matrix.scale(.3,.25,.4);
+  snorlaxsLeftArm.matrix.translate(-1,2.5,.9);
+  snorlaxsLeftArm.color = snorlaxColor;
+  snorlaxsLeftArm.render();
+
+  var snorlaxsRightArm = new Cube();
+  snorlaxsRightArm.matrix = new Matrix4(snorlaxBody.matrix);
+  snorlaxsRightArm.matrix.rotate(0,0,0,1);
+  snorlaxsRightArm.matrix.scale(.3,.25,.4);
+  snorlaxsRightArm.matrix.translate(3.33,2.5,.9);
+  snorlaxsRightArm.color = snorlaxColor;
+  snorlaxsRightArm.render();
+  
+  var snorlaxsLeftFoot = new Cube();
+  snorlaxsLeftFoot.matrix = new Matrix4(snorlaxBody.matrix);
+  snorlaxsLeftFoot.matrix.rotate(0,0,0,1);
+  snorlaxsLeftFoot.matrix.scale(.33,.33,.2);
+  snorlaxsLeftFoot.matrix.translate(0,.005,-1);
+  snorlaxsLeftFoot.color = snorlaxFeetColor;
+  snorlaxsLeftFoot.render();
+
+  var snorlaxsLFLeftToe = new Cube();
+  snorlaxsLFLeftToe.matrix = new Matrix4(snorlaxsLeftFoot.matrix);
+  snorlaxsLFLeftToe.matrix.rotate(0,0,0,1);
+  snorlaxsLFLeftToe.matrix.scale(.2,.2,.2);
+  snorlaxsLFLeftToe.matrix.translate(.5,5,0);
+  snorlaxsLFLeftToe.color = snorlaxNailColor;
+  snorlaxsLFLeftToe.render();
+
+  var snorlaxsLFMiddleToe = new Cube();
+  snorlaxsLFMiddleToe.matrix = new Matrix4(snorlaxsLeftFoot.matrix);
+  snorlaxsLFMiddleToe.matrix.rotate(0,0,0,1);
+  snorlaxsLFMiddleToe.matrix.scale(.2,.2,.2);
+  snorlaxsLFMiddleToe.matrix.translate(2.1,5,0);
+  snorlaxsLFMiddleToe.color = snorlaxNailColor;
+  snorlaxsLFMiddleToe.render();
+
+  var snorlaxsLFRightToe = new Cube();
+  snorlaxsLFRightToe.matrix = new Matrix4(snorlaxsLeftFoot.matrix);
+  snorlaxsLFRightToe.matrix.rotate(0,0,0,1);
+  snorlaxsLFRightToe.matrix.scale(.2,.2,.2);
+  snorlaxsLFRightToe.matrix.translate(3.66,5,0);
+  snorlaxsLFRightToe.color = snorlaxNailColor;
+  snorlaxsLFRightToe.render();
+
+  var snorlaxsRightFoot = new Cube();
+  snorlaxsRightFoot.matrix = new Matrix4(snorlaxBody.matrix);
+  snorlaxsRightFoot.matrix.rotate(0,0,0,1);
+  snorlaxsRightFoot.matrix.scale(.33,.33,.2);
+  snorlaxsRightFoot.matrix.translate(2.05,.005,-1);
+  snorlaxsRightFoot.color = snorlaxFeetColor;
+  snorlaxsRightFoot.render();
+
+  var snorlaxsRFLeftToe = new Cube();
+  snorlaxsRFLeftToe.matrix = new Matrix4(snorlaxsRightFoot.matrix);
+  snorlaxsRFLeftToe.matrix.rotate(0,0,0,1);
+  snorlaxsRFLeftToe.matrix.scale(.2,.2,.2);
+  snorlaxsRFLeftToe.matrix.translate(.5,5,0);
+  snorlaxsRFLeftToe.color = snorlaxNailColor;
+  snorlaxsRFLeftToe.render();
+
+  var snorlaxsRFMiddleToe = new Cube();
+  snorlaxsRFMiddleToe.matrix = new Matrix4(snorlaxsRightFoot.matrix);
+  snorlaxsRFMiddleToe.matrix.rotate(0,0,0,1);
+  snorlaxsRFMiddleToe.matrix.scale(.2,.2,.2);
+  snorlaxsRFMiddleToe.matrix.translate(2.1,5,0);
+  snorlaxsRFMiddleToe.color = snorlaxNailColor;
+  snorlaxsRFMiddleToe.render();
+
+  var snorlaxsRFRightToe = new Cube();
+  snorlaxsRFRightToe.matrix = new Matrix4(snorlaxsRightFoot.matrix);
+  snorlaxsRFRightToe.matrix.rotate(0,0,0,1);
+  snorlaxsRFRightToe.matrix.scale(.2,.2,.2);
+  snorlaxsRFRightToe.matrix.translate(3.66,5,0);
+  snorlaxsRFRightToe.color = snorlaxNailColor;
+  snorlaxsRFRightToe.render();
+
+  var snorlaxsRightPaw = new Cube();
+  snorlaxsRightPaw.matrix = new Matrix4(snorlaxsRightFoot.matrix);
+  snorlaxsRightPaw.matrix.rotate(0,0,0,1);
+  snorlaxsRightPaw.matrix.scale(.65,.5,.2);
+  snorlaxsRightPaw.matrix.translate(0.275,.15,-1);
+  snorlaxsRightPaw.color = snorlaxPawColor;
+  snorlaxsRightPaw.render();
+
+  var snorlaxsLeftPaw = new Cube();
+  snorlaxsLeftPaw.matrix = new Matrix4(snorlaxsLeftFoot.matrix);
+  snorlaxsLeftPaw.matrix.rotate(0,0,0,1);
+  snorlaxsLeftPaw.matrix.scale(.65,.5,.2);
+  snorlaxsLeftPaw.matrix.translate(0.275,.15,-1);
+  snorlaxsLeftPaw.color = snorlaxPawColor;
+  snorlaxsLeftPaw.render();
 }
 
 // set the text of an HTML element
